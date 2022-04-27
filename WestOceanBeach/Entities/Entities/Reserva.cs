9ee29dd35af1c208cc1c5900ba21fd75281e174c
byronRC10 <byronredondo29@gmail.com>
2022-04-26 20:22:29 -0600

    Para reservas

---
+++ WestOceanBeach/Entities/Entities/Reserva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Entities
+{
+    public class Reserva
+    {
+        public DateTime fechaI { get; set; }
+        public DateTime fechaF { get; set; }
+        public string fechaIS { get; set; }
+        public string fechaFS { get; set; }
+        public int tipoHabitacion { get; set; }
+        public int numHabitacion { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string primerApellido { get; set; }
+        public string segundoApellido { get; set; }
+        public string correo { get; set; }
+        public string monto { get; set; }
+        public int tarjetaCredito { get; set; }
+
+    }// fin clase
+
+}// fin
