df2c658d0364a1885586d7f86170da2c5b5aa22d
Valeska Molina Rodriguez <85156700+ValeskaMolina@users.noreply.github.com>
2022-05-13 11:31:41 -0600

    US12: Editar SobreNoosotros
    (CSS,JS,HTML)Se crea botones para validar los campos con javascript.

---
+++ WestOceanBeach/Presentacion/wwwroot/js/admin.js
@@ -0,0 +1,34 @@
+﻿
+
+
+
+
+
+function updateAboutUs() {
+   
+
+    var text = document.getElementById('aboutUs') ;
+
+    if (trimfield(text.value) == '') {
+      
+        var aswer = document.getElementById('answer');
+        aswer.innerHTML = "Complete el campo no puede ir vacio";
+        var modal = document.getElementById("myModal");
+        modal.style.display = "block";
+        
+    } else {
+
+
+
+
+
+
+    }
+
+
+
+}
+
+function trimfield(str) {
+    return str.replace(/^\s+|\s+$/g, '');
+}
