1974132fb56e27b2bddc94cdfae475f9ce52ee1a
Valeska Molina Rodriguez <85156700+ValeskaMolina@users.noreply.github.com>
2022-05-16 09:29:04 -0600

    CargarIMagenesRoot
    Se intenta cargar imagenes para que se almacenen en el root

---
+++ WestOceanBeach/Entities/Entities/Imagenes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Entities
+{
+    public  class Imagenes
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string full_path { get; set; }
+
+
+    }
+}
