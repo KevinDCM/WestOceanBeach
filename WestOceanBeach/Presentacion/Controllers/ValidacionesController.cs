e577cfa1196a8b57860cf38c5cdd1f0d6073c8dd
Kathe93 <70492064+Kathe93@users.noreply.github.com>
2022-07-14 23:57:27 -0600

    Cambios

---
+++ WestOceanBeach/Presentacion/Controllers/ValidacionesController.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Presentacion.Controllers
+{
+    public class ValidacionesController : Controller
+    {
+        public IConfiguration Configuration { get; }
+        HttpClient client02 = new HttpClient();
+        private readonly IWebHostEnvironment _iwebhost;// get the project access
+
+        public ValidacionesController(IWebHostEnvironment _web) {
+            _iwebhost = _web;
+        }
+
+        [HttpGet]
+      
+
+        public IActionResult Index_Val()
+        
+        {
+            return View();
+
+        }
+
+
+    }
+}
