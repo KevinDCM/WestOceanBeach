8e0fe0679c17f1d0170eeb02234bb9d8e630e9e2
Kathe93 <70492064+Kathe93@users.noreply.github.com>
2022-07-15 10:08:44 -0600

    SubirCambios

--- WestOceanBeach/Presentacion/wwwroot/js/admin.js
+++ WestOceanBeach/Presentacion/wwwroot/js/admin.js
@@ -1182,3 +1182,19 @@ function Logout(){
 	});
 
 }
+
+ function limitDecimalPlaces(e, count) {
+  if (e.target.value.indexOf(',') == -1) { return; }
+  if ((e.target.value.length - e.target.value.indexOf(',')) > count) {
+    e.target.value = parseFloat(e.target.value).toFixed(count);
+  }
+}
+
+function isNumberKey(evt)
+{
+  var charCode = (evt.which) ? evt.which : evt.keyCode;
+  if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+    return false;
+
+  return true;
+}
